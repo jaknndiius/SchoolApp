f3d120299fd20703cefbe9b5581528a1c25b8937
JUNAisKIND <0627juna@naver.com>
2023-06-18 03:41:56 +0900

    6/17

--- app/src/main/java/io/github/jaknndiius/schoolapp/fragment/timetable/ChangeTimetableFragment.kt
+++ app/src/main/java/io/github/jaknndiius/schoolapp/fragment/timetable/ChangeTimetableFragment.kt
@@ -1,11 +1,15 @@
 package io.github.jaknndiius.schoolapp.fragment.timetable
 
+import android.app.AlertDialog
 import android.content.ClipData
 import android.content.ClipDescription
+import android.content.DialogInterface
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Point
 import android.graphics.drawable.ColorDrawable
+import android.os.Binder
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.DragEvent
@@ -13,13 +17,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.view.animation.AnimationUtils
 import android.widget.*
 import android.widget.LinearLayout.LayoutParams
+import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.AppCompatButton
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.allViews
+import androidx.core.view.children
 import androidx.core.view.get
+import androidx.core.view.marginBottom
 import androidx.fragment.app.Fragment
 import io.github.jaknndiius.schoolapp.MainActivity
 import io.github.jaknndiius.schoolapp.R
+import io.github.jaknndiius.schoolapp.database.SubjectTable
 import io.github.jaknndiius.schoolapp.database.WeekDay
 import io.github.jaknndiius.schoolapp.fragment.TimetableFragment
 import kotlinx.coroutines.CoroutineScope
@@ -27,42 +38,55 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.util.*
+import kotlin.collections.ArrayList
 
 class ChangeTimetableFragment(
     private val timetableFragment: TimetableFragment
 ) : Fragment() {
 
+    private val weekday = listOf("월", "화", "수", "목", "금")
+
+    lateinit var binding: View
     lateinit var inflater: LayoutInflater
+
+    lateinit var subjectLayout: LinearLayout
+    lateinit var insertBlank: View
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         this.inflater = inflater
 
-        val binding = inflater.inflate(R.layout.timetable_setting_change_timetable, container, false)
+        binding = inflater.inflate(R.layout.timetable_setting_change_timetable, container, false)
 
         binding.findViewById<AppCompatButton>(R.id.back_button).setOnClickListener {
             timetableFragment.openTimetableSetting(TimetableFragment.Direction.PREVIOUS)
         }
 
-        val subjectLayout: LinearLayout = binding.findViewById(R.id.subject_layout)
+        subjectLayout = binding.findViewById<LinearLayout>(R.id.subject_layout).apply {
+            setOnDragListener{ v, e ->
+                when (e.action) {
+                    DragEvent.ACTION_DRAG_STARTED -> e.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
+                    DragEvent.ACTION_DRAG_ENTERED ->  true
+                    DragEvent.ACTION_DRAG_LOCATION -> true
+                    DragEvent.ACTION_DRAG_EXITED -> true
+                    DragEvent.ACTION_DROP -> true
+                    DragEvent.ACTION_DRAG_ENDED -> {
+                        if(!e.result) Toast.makeText(context, getString(R.string.request_right_drag), Toast.LENGTH_SHORT).show()
+                        this.removeView(insertBlank)
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
+        insertBlank = makeBlank()
 
         val radioGroup: RadioGroup = binding.findViewById(R.id.day_radio_group)
-        radioGroup.setOnCheckedChangeListener { radioGroup, id ->
-            reloadDaySubject(
-                when(id) {
-                    R.id.monday -> WeekDay.MONDAY
-                    R.id.tuesday -> WeekDay.TUESDAY
-                    R.id.wednesday -> WeekDay.WENDESDAY
-                    R.id.thursday -> WeekDay.THURSDAY
-                    R.id.friday -> WeekDay.FRIDAY
-                    else -> WeekDay.MONDAY
-                },
-                subjectLayout
-            )
+        radioGroup.setOnCheckedChangeListener { _, id ->
+            reloadDaySubject(getWeekdayWithId(id))
         }
         binding.findViewById<RadioButton>(R.id.monday).isChecked = true
 
@@ -72,8 +96,8 @@ class ChangeTimetableFragment(
             binding.findViewById(R.id.drop_subject_layout_line3)
         )
         CoroutineScope(Dispatchers.IO).launch {
-            val subjects = MainActivity.subjectManager.getAll()
 
+            val subjects = MainActivity.subjectManager.getAll()
             withContext(Dispatchers.Main) {
                 subjects.forEachIndexed { index, subject ->
                     dropSubjectLayouts[index%3].let {
@@ -82,17 +106,75 @@ class ChangeTimetableFragment(
                 }
             }
         }
+        reloadDaySubject(WeekDay.MONDAY)
 
+        binding.findViewById<AppCompatButton>(R.id.save_button).setOnClickListener {
+            val currentWeekDay =  weekday[getCheckedWeekday().ordinal]
+            askAndRun(
+                getString(R.string.ask_save_at_weekday, currentWeekDay), DialogInterface.OnClickListener { _, _ ->
+                    save(getCheckedWeekday())
+                    Toast.makeText(context, getString(R.string.say_saved_subjects_with_weekday, currentWeekDay), Toast.LENGTH_LONG).show()
+                })
+        }
+
+        binding.findViewById<AppCompatButton>(R.id.init_button).setOnClickListener {
+            askAndRun(
+                getString(R.string.ask_really_init), DialogInterface.OnClickListener { _, _ ->
+                    reloadDaySubject(getCheckedWeekday())
+                    Toast.makeText(context, getString(R.string.say_initialized_at_weekday), Toast.LENGTH_LONG).show()
+                })
+        }
         return binding
     }
 
-    private fun reloadDaySubject(weekDay: WeekDay, container: ViewGroup) {
-        container.removeAllViews()
+    private fun askAndRun(message: String, onYesListener: DialogInterface.OnClickListener) {
+        AlertDialog.Builder(context)
+            .setMessage(message)
+            .setPositiveButton(getString(R.string.yes), onYesListener)
+            .setNegativeButton(getString(R.string.no),  null)
+            .show()
+    }
+
+    private fun getWeekdayWithId(id: Int): WeekDay {
+        return when(id) {
+            R.id.monday -> WeekDay.MONDAY
+            R.id.tuesday -> WeekDay.TUESDAY
+            R.id.wednesday -> WeekDay.WENDESDAY
+            R.id.thursday -> WeekDay.THURSDAY
+            R.id.friday -> WeekDay.FRIDAY
+            else -> WeekDay.MONDAY
+        }
+    }
+    private fun getCheckedWeekday(): WeekDay {
+        return getWeekdayWithId(binding.findViewById<RadioGroup>(R.id.day_radio_group).checkedRadioButtonId)
+    }
+
+    private fun save(weekDay: WeekDay) {
         CoroutineScope(Dispatchers.IO).launch {
-            val todaySubjects = MainActivity.subjectTableManager.get(weekDay).subjects
+            MainActivity.subjectTableManager.addOrUpdate(weekDay, getSubjectsSet())
+        }
+    }
+
+    private fun getSubjectsInTable(): List<View> {
+        val outputViews: ArrayList<View> = arrayListOf()
+        binding.findViewById<LinearLayout>(R.id.subject_layout).findViewsWithText(outputViews, "SubjectTableView", LinearLayout.FIND_VIEWS_WITH_CONTENT_DESCRIPTION)
+        return outputViews
+    }
+
+    private fun getSubjectsSet(): List<String> {
+        return getSubjectsInTable().map { it.findViewById<TextView>(R.id.subject_name).text.toString() }
+    }
+
+    private fun reloadDaySubject(weekDay: WeekDay) {
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val tables = MainActivity.subjectTableManager.get(weekDay)
+
             withContext(Dispatchers.Main) {
-                todaySubjects.forEach {
-                    container.addView(makeTableSubject(container, it.name))
+                subjectLayout.removeAllViews()
+
+                tables.subjects.forEach {
+                    subjectLayout.addView(makeTableSubject(it.name))
                 }
             }
         }
@@ -127,54 +209,84 @@ class ChangeTimetableFragment(
         }
     }
 
-    private fun removeSubject(view: View, container: ViewGroup) {
-        container.removeView(view)
+    private fun removeSubject(view: View) {
+        subjectLayout.removeView(view)
     }
 
-    private fun makeTableSubject(container: ViewGroup, subjectName: String): View {
-
-        fun setMarginBottom(view: View, margin: Int) {
-            val par = view.layoutParams as LayoutParams
-            par.bottomMargin = margin
-            view.layoutParams = par
+    private fun makeBlank(): View {
+        return makeSubject(subjectLayout, "+").apply {
+            setOnDragListener{ v, e ->
+                when (e.action) {
+                    DragEvent.ACTION_DRAG_STARTED -> e.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
+                    DragEvent.ACTION_DRAG_ENTERED ->  {
+                        v.findViewById<LinearLayout>(R.id.subject_background).startAnimation(AnimationUtils.loadAnimation(context, R.anim.scale_down))
+                        true
+                    }
+                    DragEvent.ACTION_DRAG_LOCATION -> true
+                    DragEvent.ACTION_DROP -> {
+                        val item: ClipData.Item = e.clipData.getItemAt(0)
+                        val dragData = item.text
+                        subjectLayout.addView(
+                            makeTableSubject(dragData.toString()),
+                            subjectLayout.indexOfChild(v)
+                        )
+                        subjectLayout.removeView(v)
+                        true
+                    }
+                    DragEvent.ACTION_DRAG_EXITED -> {
+                        v.findViewById<LinearLayout>(R.id.subject_background).startAnimation(AnimationUtils.loadAnimation(context, R.anim.scale_up))
+                        true
+                    }
+                    DragEvent.ACTION_DRAG_ENDED -> true
+                    else -> false
+                }
+            }
         }
+    }
 
-        return makeSubject(container, subjectName).apply {
+
+    private fun makeTableSubject(subjectName: String): View {
+
+        return makeSubject(subjectLayout, subjectName).apply {
+            contentDescription = "SubjectTableView"
             setOnLongClickListener {
-                removeSubject(this, container)
+                removeSubject(it)
                 true
             }
             setOnDragListener{ v, e ->
                 when (e.action) {
                     DragEvent.ACTION_DRAG_STARTED -> e.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
                     DragEvent.ACTION_DRAG_ENTERED ->  {
-                        setMarginBottom(this, 90)
+                        v.findViewById<LinearLayout>(R.id.subject_background).startAnimation(AnimationUtils.loadAnimation(context, R.anim.scale_down))
                         true
                     }
                     DragEvent.ACTION_DRAG_LOCATION -> {
+                        subjectLayout.removeView(insertBlank)
+                        val currentIdx = subjectLayout.indexOfChild(v)
+                        subjectLayout.addView(insertBlank,
+                            if(e.y <= v.height /2) currentIdx else currentIdx+1)
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                            binding.findViewById<ScrollView>(R.id.scroll_subjects).scrollToDescendant(insertBlank)
+                        }
                         true
                     }
                     DragEvent.ACTION_DRAG_EXITED -> {
-                        setMarginBottom(this, 30)
+                        v.findViewById<LinearLayout>(R.id.subject_background).startAnimation(AnimationUtils.loadAnimation(context, R.anim.scale_up))
                         true
                     }
                     DragEvent.ACTION_DROP -> {
-
-                        setMarginBottom(this, 30)
-
+                        subjectLayout.removeView(insertBlank)
                         val item: ClipData.Item = e.clipData.getItemAt(0)
                         val dragData = item.text
-
-                        container.addView(
-                            makeTableSubject(container, dragData.toString()),
-                            container.indexOfChild(this) +1
+                        subjectLayout.addView(
+                            makeTableSubject(dragData.toString()),
+                            subjectLayout.indexOfChild(v)
                         )
+                        subjectLayout.removeView(v)
                         true
                     }
-                    DragEvent.ACTION_DRAG_ENDED -> {
-                        if(!e.result) Toast.makeText(context, "시간표 안에 과목을 드래그 해주세요.", Toast.LENGTH_LONG).show()
-                        true
-                    }
+                    DragEvent.ACTION_DRAG_ENDED -> true
                     else -> false
                 }
             }
